774fc2aee566f441d60c0633c29949fedc4ad0a1
VicAffile <victoraffile@gmail.com>
2021-04-13 18:02:11 +0200

    qui suis je contenu

--- js/qui_suis_je.js
+++ js/qui_suis_je.js
@@ -7,7 +7,14 @@ document.getElementById('qui_suis_je_job_titre').querySelectorAll('span')[1].sty
 
 function fleche(e) {
     let fleche = e.querySelectorAll("span");
+    let texte;
+    for (let i = 0; i < e.parentNode.childNodes.length; i++) {
+        if (e.parentNode.childNodes[i] == e) {
+            texte = i + 2;
+        }
+    }
     let etat = fleche[0].style.display;
     fleche[0].style.display = fleche[1].style.display;
     fleche[1].style.display = etat;
+    e.parentNode.childNodes[texte].style.display = etat;
 }
