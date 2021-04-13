6ab247eedf6535f301b09d003cd1190ca19201d3
VicAffile <victoraffile@gmail.com>
2021-04-13 17:22:17 +0200

    fleche clique

---
+++ js/qui_suis_je.js
@@ -0,0 +1,13 @@
+document.getElementById('qui_suis_je_presentation_titre').querySelectorAll('span')[0].style.display = 'contents';
+document.getElementById('qui_suis_je_presentation_titre').querySelectorAll('span')[1].style.display = 'none';
+document.getElementById('qui_suis_je_passion_titre').querySelectorAll('span')[0].style.display = 'contents';
+document.getElementById('qui_suis_je_passion_titre').querySelectorAll('span')[1].style.display = 'none';
+document.getElementById('qui_suis_je_job_titre').querySelectorAll('span')[0].style.display = 'contents';
+document.getElementById('qui_suis_je_job_titre').querySelectorAll('span')[1].style.display = 'none';
+
+function fleche(e) {
+    let fleche = e.querySelectorAll("span");
+    let etat = fleche[0].style.display;
+    fleche[0].style.display = fleche[1].style.display;
+    fleche[1].style.display = etat;
+}
