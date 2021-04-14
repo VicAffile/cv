c78cec17ccca488f89f470ae326af5a1ae138274
VicAffile <victoraffile@gmail.com>
2021-04-15 00:34:42 +0200

    qui suis je js opti

--- js/header.js
+++ js/header.js
@@ -1,10 +1,7 @@
 function afficher_menu() {
-    console.log('yo')
     if (document.getElementById('etiquette').style.right == '-200px') {
-        console.log('yes');
         document.getElementById('etiquette').style.right = '0px'
     } else if (document.getElementById('etiquette').style.right == '0px') {
-        console.log('no')
         document.getElementById('etiquette').style.right = '-200px'
     }
 }
--- js/qui_suis_je.js
+++ js/qui_suis_je.js
@@ -1,12 +1,3 @@
-document.getElementById('qui_suis_je_presentation_titre').querySelectorAll('span')[0].style.display = 'contents';
-document.getElementById('qui_suis_je_presentation_titre').querySelectorAll('span')[1].style.display = 'none';
-document.getElementById('qui_suis_je_passion_titre').querySelectorAll('span')[0].style.display = 'contents';
-document.getElementById('qui_suis_je_passion_titre').querySelectorAll('span')[1].style.display = 'none';
-document.getElementById('qui_suis_je_job_titre').querySelectorAll('span')[0].style.display = 'contents';
-document.getElementById('qui_suis_je_job_titre').querySelectorAll('span')[1].style.display = 'none';
-document.getElementById('qui_suis_je_ecole_titre').querySelectorAll('span')[0].style.display = 'contents';
-document.getElementById('qui_suis_je_ecole_titre').querySelectorAll('span')[1].style.display = 'none';
-
 function fleche(e) {
     const fleches = e.querySelectorAll('span');
     const etat = fleches[0].style.display;
